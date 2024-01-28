8ed53e75206c8c100d698d529a0c4c6702e8c54c
Carlos Patino <carlospatinos@gmail.com>
2024-01-28 09:52:32 +0000

    QR generation

--- routes/download.js
+++ routes/download.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const QRCode = require('qrcode')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('download', {});
 });
 
-router.post('/', function(req, res, next) {
-  res.render('download', {});
+router.post('/', function (req, res, next) {
+
+  let data = {
+    name: "Employee Name",
+    age: 27,
+    department: "Police",
+    id: "aisuoiqu3234738jdhf100223"
+  }
+
+  let stringdata = JSON.stringify(data)
+
+  QRCode.toDataURL(stringdata, function (err, qrEncodedCode) {
+    if (err) return console.log("error occurred")
+    // Printing the code
+    console.log(qrEncodedCode)
+    res.render('download', {qr: qrEncodedCode});
+  })
+  
 });
 
 module.exports = router;
